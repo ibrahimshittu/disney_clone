94736eb1cfd9e8457a0235ca6bf7792b38e74356
Ibrahim Shittu <ibshittu01@gmail.com>
2021-12-16 01:09:09 +0100

    start

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header() {
+    return (
+        <Container>
+            Header 
+        </Container>
+    )
+}
+
+export default Header
+
+const Container = styled.div`
+    background: #090b13;
+    height: 70px;
+`
