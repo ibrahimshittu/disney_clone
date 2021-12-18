0e4503c5eeba17e0bca9c356744973a5ae3b1a81
Ibrahim Shittu <ibshittu01@gmail.com>
2021-12-18 11:07:31 +0100

    header :)

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,25 +4,109 @@ import styled from 'styled-components'
 function Header() {
     return (
         <Nav>
-            <Logo src="" />
+            <Logo src="/images/logo.svg" />
             <NavMenu>
+                <a >
+                    <img src="/images/home-icon.svg" alt="home-icon"/>
+                    <span>HOME</span>
+                </a>
+                <a >
+                    <img src="/images/search-icon.svg" alt="search-icon"/>
+                    <span>SEARCH</span>
+                </a>
+                <a >
+                    <img src="/images/watchlist-icon.svg" alt="watchlist-icon"/>
+                    <span>WATCHLIST</span>
+                </a>
+                <a >
+                    <img src="/images/original-icon.svg" alt="originals-icon"/>
+                    <span>ORIGINALS</span>
+                </a>
+                <a >
+                    <img src="/images/movie-icon.svg" alt="movie-icon"/>
+                    <span>MOVIES</span>
+                </a>
+                <a >
+                    <img src="/images/series-icon.svg" alt="series-icon"/>
+                    <span>SERIES</span>
+                </a>
 
             </NavMenu>
+            <UserImg src="/images/login-background.jpg" />
         </Nav>
     )
 }
 
 export default Header
 
-const Nav = styled.div`
+const Nav = styled.nav`
     background: #090b13;
     height: 70px;
+    display: flex;
+    align-items: center;
+    padding: 0 36px;
 `
 
-const Logo = styled.div`
+const Logo = styled.img`
+    width: 80px;
+    cursor: pointer;
     
 `
 
 const NavMenu = styled.div`
+    display: flex;
+    flex: 1;
+    margin-left: 25px;
+    align-items: center;
     
+
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+        cursor: pointer;
+
+
+        img {
+            height: 20px;
+        }
+
+        span {
+            font-size: 12px;
+            letter-spacing: 1.42px;
+            position: relative;
+
+            &:after {
+                content: "";
+                height: 2px;
+                position: absolute;
+                background: white;
+                left: 0;
+                right: 0;
+                bottom: -6px;
+                opacity: 0;
+                transform-origin: left center;
+                transform: scaleX(0);
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s ;
+            }
+        }
+
+        &:hover {
+            span:after {
+                transform: scaleX(1);
+                opacity: 1;
+
+            }
+        }
+
+    }
+    
+`
+
+const UserImg = styled.img`
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    cursor: pointer;
+
 `
---
+++ src/components/ImgSlider.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function ImgSlider() {
+    return (
+        <Container>
+            Image slider
+        </Container>
+    )
+}
+
+export default ImgSlider
+
+const Container = styled.div`
+`
