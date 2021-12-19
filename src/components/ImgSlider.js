a9fdff0907982bc2d9874f1c527532fb8d105f82
Ibrahim Shittu <ibshittu01@gmail.com>
2021-12-19 02:23:17 +0100

    header done

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -36,8 +36,12 @@ const Carousel = styled(Slider)`
     ul li button {
         &:before {
             font-size: 10px;
-            color: white ;
+            color: rgb(150, 158, 171);
         }
+    }
+
+    li.slick-active button:before {
+        color: white;
     }
 
     .slick-list {
@@ -51,11 +55,18 @@ const Carousel = styled(Slider)`
 `
 
 const Wrap = styled.div`
+    cursor: pointer;
     img {
+        border: 4px solid transparent;
         border-radius: 4px;
         height: 100%;
         width: 100%;
         box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
             rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+        transition-duration: 300ms;
+
+        &:hover {
+            border: 4px solid rgba( 249, 249, 249, 0.8);
+        }
     }
 `
