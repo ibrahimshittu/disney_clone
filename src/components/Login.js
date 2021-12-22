5dc320eadfee057190a7f78f7e701f657304aa23
Ibrahim Shittu <ibshittu01@gmail.com>
2021-12-22 12:48:38 +0100

    login b0dy

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,12 @@ function Login() {
             <CTA>
                 <CTALogoOne src="/images/cta-logo-one.svg" />
                 <SignIn>GET ALL THERE</SignIn>
+                <Descr>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+                    magna aliqua. quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+                    Ut enim ad minim veniam
+                </Descr>
+                <CTALogoTwo src="/images/cta-logo-two.png" />
                  
             </CTA>
             
@@ -20,7 +26,7 @@ const Container = styled.div`
     position: relative;
     height: calc(100vh - 70px);
     display: flex;
-    align-items: center;
+    align-items: top;
     justify-content: center;
 
     &:before {
@@ -34,14 +40,16 @@ const Container = styled.div`
         right: 0;
         left: 0;
         bottom: 0;
+        opacity: 0.6;
         z-index: -1;
     }
 
 `
 
 const CTA = styled.div`
-    width: 60%;
+    width: 90%;
     max-width: 650px;
+    margin-top: 60px;
     padding: 80px 40px;
     display: flex;
     flex-direction: column;
@@ -52,9 +60,8 @@ const CTA = styled.div`
 
 const CTALogoOne = styled.img`
     
-    
-
 `
+
 
 const SignIn = styled.a`
     width: 100%;
@@ -62,7 +69,7 @@ const SignIn = styled.a`
     padding: 17px 0;
     color: #f9f9f9;
     font-weight: bold;
-    borddr-radius: 4px;
+    border-radius: 4px;
     text-align: center;
     font-size: 18px;
     cursor: pointer;
@@ -76,4 +83,13 @@ const SignIn = styled.a`
     }
     
 `
-
+const Descr = styled.div`
+    font-size: 11px;
+    text-align: center; 
+    letter-spacing: 1.5px;
+    line-height: 1.5;  
+    margin-bottom: 12px; 
+`
+const CTALogoTwo = styled.img`
+    
+`
